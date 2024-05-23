46acdd13b35c3ed76e720aac338415abe2cf530c
Vitalii <snaiper_1995@mail.ru>
2024-05-23 22:02:16 +1000

    1234

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-import './_promo.js';
-import './_curtain.js';
-import './_repair.js';
-import './_includes.js';
-import './_questions.js';
-import './_order_cleaning.js';
-import './_apartmentcleaning__price.js';
+import './promo.js';
+import './curtain.js';
+import './repair.js';
+import './includes.js';
+import './questions.js';
+import './order_cleaning.js';
+import './apartmentcleaning__price.js';
